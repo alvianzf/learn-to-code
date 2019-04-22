40c7ecc7a054af85edfab1b7acd0a218a0a31673
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-04-22 17:55:12 +0700

    lesson 5

---
+++ algorithm/lesson_5.js
@@ -0,0 +1,22 @@
+// lesson 5
+
+//increment a number
+
+/**
+ * to increment a number, commonly 2 ways can be done:
+ * add the variable with one
+ * 
+ * using shorthand notations
+ */
+
+ let number = 1
+
+ // example 1
+
+ number = number +1
+ console.log(number)
+
+ // example 2
+
+ number += 1
+ console.log(number)
