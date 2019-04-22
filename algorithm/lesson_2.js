02678d5a379e1d47e3ee7233259389540923bbe5
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-04-22 17:27:19 +0700

    lesson 2

---
+++ algorithm/lesson_2.js
@@ -0,0 +1,10 @@
+// lesson 2: let, const, var
+
+const a = 12;
+// const means that the value cannot be changed
+
+var b = 13;
+// var means that the value and tha variable name can be changed later on
+
+let c = 14;
+// let means that only the value can be changed, and no other variables with the same name can exist
