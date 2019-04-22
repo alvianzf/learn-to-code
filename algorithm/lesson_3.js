67732a1ee9e5219af01937d5a4137fd1a945d9f1
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-04-22 17:35:24 +0700

    lesson 3

---
+++ algorithm/lesson_3.js
@@ -0,0 +1,19 @@
+// lesson 3
+
+/**
+ * a console.log is a powerful debugging tool that will help you in more ways than one
+ * it is used to print out anything, from a variable, to an abject to your console in realtime!
+ */
+
+ // we define a variable first:
+
+ let str = 'Coding is fun?'
+
+ console.log(str)
+ // this will print out the value of the str variable
+
+ //let's change the content
+
+ str = 'Yes, it is!'
+
+ console.log(str)
