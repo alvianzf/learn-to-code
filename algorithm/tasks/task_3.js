0dd309ffd15e355759f9a4291053db289cda4ed5
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-04-23 06:57:45 +0700

    task 3

---
+++ algorithm/tasks/task_3.js
@@ -0,0 +1,15 @@
+// Sort the array
+
+/**
+ * 
+ * Objective: sort the array in an orderly, ascending manner
+ * try not to use the sort() function for this assignment
+ * 
+ * Hint: Use for and if statement to accomplish this assignment
+ */
+
+ var unsortedArray = [3,5,1,8,6,2,6,4,9]
+
+ /**
+  * put your code below this lineg
+  */
