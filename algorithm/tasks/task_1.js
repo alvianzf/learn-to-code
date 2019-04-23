1a44c2c7a59a4cc3faf36975b7e6cfcee56a373c
Alvian Zachry Faturrahman <alvianzf@gmail.com>
2019-04-23 12:38:47 +0700

    task 1

--- algorithm/tasks/task_1.js
+++ algorithm/tasks/task_1.js
@@ -4,12 +4,12 @@
 
 /**
  * Objective: find the odd numbers of the elements in the array
+ *
+ * Hint: try to use for loop and modulus for this assignment
  */
 
 const theArray = [3123,1244,6463,3940,9123,1246]
 
-/**  
- * 
+/**
  * put your code below this line
- * 
  */
