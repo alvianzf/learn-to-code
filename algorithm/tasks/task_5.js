f23179a477469d3eccf9bbed84875bf349255698
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-04-23 13:44:13 +0700

    taks 5

---
+++ algorithm/tasks/task_5.js
@@ -0,0 +1,20 @@
+// task 5
+
+// the Fibonacci sequence
+
+/**
+ * 
+ * Objective:
+ * To print out the first 100 Fibonacci sequence on the console
+ * 
+ * Fibonacci is expressed with the function
+ * 
+ * c = (a-1)+a
+ * 
+ * Or, the next sequence is the sum of two of the previous numbers
+ * 
+ * ex: 1, 1, 2, 3, 5, 8, 13, 21
+ * 
+ * 
+ * Put your code below this line
+ */
