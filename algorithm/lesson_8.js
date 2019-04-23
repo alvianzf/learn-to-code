a27490271a201295f1b5c4176def66071f91d705
Alvian Zachry Faturrahman <alvianzf@macbooks-MacBook-Air.local>
2019-04-23 14:46:45 +0700

    lesson 8

--- algorithm/lesson_8.js
+++ algorithm/lesson_8.js
@@ -5,9 +5,9 @@
 /**
  * Sometimes we want to know how long a string is
  * or how many things are there in an array
- * we cal use the length() function to sort this out
+ * we cal use the .length function to sort this out
  */
 
  const str = 'lorem ipsum dolor sit amet'
 
- console.log(length(str))
+ console.log(str.length)
